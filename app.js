29e330efd6461e17ed7d2077985d8d00965c891f
gDimIn <mep3ab4uk1009@gmail.com>
2023-12-06 16:10:11 +0200

    Try two

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 
-const EventEmitter = require('events')
 const express = require('express')
 const path = require('path')
 const app = express()
 
-const PORT = 9005
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
-app.set('views',path.join(__dirname, 'public/views'))
+app.use(express.static('public'))
+//app.set('views',path.join(__dirname, 'views'))
+
+const indexRoute = require("./routes/index")
+app.use('/', indexRoute)
 
 let num = 5
 let names = ["Dima", "Oleg","Julia","Katya","Sveta"] 
 
 
-app.get('/', (req, res) =>{
+/*app.get('/', (req, res) =>{
     const data = {
         btn1: 'capuch',
         btn2: 'late'
@@ -23,14 +26,8 @@ app.get('/', (req, res) =>{
 
 app.get('/', (req, res) =>{
     
-})
-
-app.use(express.static(path.join(__dirname,'public')))
+})*/
 
 app.listen(PORT, ()=>{
     console.log(`Server started: https://lukavadu.web.app:${PORT}`)
 })
-
-/*fs.writeFile("./testjson.txt", "My string", () => {
-    console.log('names: %s', names)
-})*/
