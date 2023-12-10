ec3ce738c22e97e1e0476bdcb92ff7370323e0f5
gDimIn <mep3ab4uk1009@gmail.com>
2023-12-10 15:11:13 +0200

    fix tree

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-/*const root = ReactDOM.createRoot( document.getElementById('root') )
-const elements = <input placeholder='Any text'/>
+let inc = 0;
 
-root.render(elements)*/
+function TestApp() {
+
+  return (  <div className='btnClick'>
+    <button onClick={increment}>ClickMe</button>
+    <label id='label'> {inc} </label>
+    </div> )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+//const label = ReactDOM.document.getElementById('label');
+/*root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+);*/
+root.render(<TestApp/>);
 
+function increment(){
+  console.log(inc);
+  inc= inc + 1;
+  root.render(<TestApp/>);
+};
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
